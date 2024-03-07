4cefe3d5e946d2837af0c98ec58bac840ba6c8c3
Enes Demirbaş <enesdemirbasoffical@gmail.com>
2024-03-07 22:21:48 +0300

    Assets Added

---
+++ Assets/_Project/Runtime/Project/Launcher/Menager/ScreenLayers.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenLayers 
+{
+    public const string Layer1="Layer1";
+    public const string Layer2="Layer1";
+    public const string Layer3="Layer1";
+    public const string Layer4="Layer1";
+}
