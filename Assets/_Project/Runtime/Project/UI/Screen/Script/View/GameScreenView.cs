de50e5f74947be10cd75da7dfb6242f96b9393e4
Ömer Faruk Kavak <kvkomerfaruk@gmail.com>
2024-03-11 14:35:09 +0300

    DoTween added to gameasembly file

--- Assets/_Project/Runtime/Project/UI/Screen/Script/View/GameScreenView.cs
+++ Assets/_Project/Runtime/Project/UI/Screen/Script/View/GameScreenView.cs
@@ -1,12 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening; //Do tween implement
+using DG.Tweening;
+using UnityEngine.UI;
+using Image = UnityEngine.UIElements.Image; //Do tween implement
 
 public class GameScreenView : MonoBehaviour
 {
+    /*public Image Image1;
+    private Sequence _sequence;
+
+
+    private void Awake()
+    {
+        _sequence = DOTween.Sequence();
+
+    }
+
+
+    public void OnClick()
+    {
+        //Image1.color = Color.red;
+        Image1.DOFade(0, 0).SetEase(Ease.InQuad).OnComplete((() =>
+        {
+            Image1.DOFade(1, .4f).SetEase(Ease.InQuad);
+        }));
+        var tween1 = Image1.transform.DOScaleY(13, .4f).SetRelative(true);
+        var tween2 = Image1.transform.DOScaleX( 7,.3f).SetRelative(true);
+        _sequence.Join(tween1).SetEase(Ease.InQuad);
+        _sequence.Join(tween2).SetEase(Ease.InQuad);
+    }*/
     public async void OnClickPauseButton()
     {
+        
         await ScreenModel.Instance.OpenScreen(ScreenKeys.PauseScreen, ScreenLayers.Layer2);
     }
     public async void OnClickBackButton()
