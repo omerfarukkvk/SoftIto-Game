f8b9cf8f2fa7f3baf65a669ec85631d45d7bce20
Ömer Faruk Kavak <kvkomerfaruk@gmail.com>
2024-03-19 22:21:52 +0300

    Character movement slider and some prefabs added

--- Assets/_Project/Runtime/Project/UI/Screen/Script/View/GameScreenView.cs
+++ Assets/_Project/Runtime/Project/UI/Screen/Script/View/GameScreenView.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Project.Runtime.Project.UI.Screen.Script.View
 {
     
     public class GameScreenView : MonoBehaviour
     {
+        public Slider MovementSlider;
         public async void OnClickPauseButton()
         {
             await ScreenModel.Instance.OpenScreen(ScreenKeys.PauseScreen, ScreenLayers.Layer1);
+        }
+        public void OnChangedMovementSlider()
+        {
+            MovementSlider.value = 0;
         }
     }
 }
