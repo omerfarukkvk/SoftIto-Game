2bd81824f2728a07110717ac4a361d2a0b1e61ee
Ömer Faruk Kavak <kvkomerfaruk@gmail.com>
2024-04-01 02:24:20 +0300

    updated

--- Assets/_Project/Runtime/Project/Levels/Scripts/Enviroment/Bomb.cs
+++ Assets/_Project/Runtime/Project/Levels/Scripts/Enviroment/Bomb.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class MummyHeadBomb : MonoBehaviour
 {
+    public bool IsMovable;
     private float _speed = 2f;
 
     void Update()
     {
-        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        if(IsMovable)
+            transform.Translate(Vector3.right * _speed * Time.deltaTime);
     }
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("girdi");
         if(other.CompareTag("Wall"))
         {
-            transform.Rotate(Vector3.up * 180);
+            _speed = -_speed;
         }
     }
 }
